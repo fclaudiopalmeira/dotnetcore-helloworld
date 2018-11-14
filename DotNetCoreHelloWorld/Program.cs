a00fbe1a8f68d33d92d95f9cf32f26868e75d5bb
fclaudiopalmeira <33045584+fclaudiopalmeira@users.noreply.github.com>
2018-11-14 15:21:36 -0300

    Update Program.cs

--- DotNetCoreHelloWorld/Program.cs
+++ DotNetCoreHelloWorld/Program.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace WebApi
@@ -12,6 +19,7 @@ namespace WebApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+				.UseUrls("http://+:5000");
     }
 }
